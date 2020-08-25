f74e742f0e802c35cbb48320c1fcb0c36532368b
NayZawAg <nayzawaung.nicky@gmail.com>
2020-08-25 17:39:15 +0630

    added login and register form design database

--- resources/views/frontendtemplate.blade.php
+++ resources/views/frontendtemplate.blade.php
@@ -108,25 +108,26 @@
                         </li>
                         @endif
                         @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        <li>
+                            <a  class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                               {{ Auth::user()->name }} <span class="caret"></span>
                           </a>
 
-                          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                              <a class="dropdown-item" href="{{ route('logout') }}"
+                          <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                            
+                            <li><a href="{{-- {{route('profilepage')}} --}}">{{ __('Profile') }}
+
+                            </a></li>
+                            <li><a href="{{ route('logout') }}"
                               onclick="event.preventDefault();
                               document.getElementById('logout-form').submit();">
                               {{ __('Logout') }}
-                          </a>              
-                          <a class="dropdown-item" href="{{route('profilepage')}}">Profile
-
-                          </a>
+                          </a></li>
 
                           <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                               @csrf
                           </form>
-                      </div>
+                      </ul>
                   </li>
                   @endguest
 
